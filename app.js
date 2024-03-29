0017c06dfd037fb42fb323214c12129738411617
Humoyin <baxranoofxumoyun@gmail.com>
2024-03-29 13:13:05 +0500

    yangi malumotlar

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // const listItem=document.getElementsByTagName('bady')
 // console.log(listItem)
-const listItem=document.getElementsByClassName('list-item')
+// const listItem=document.getElementsByClassName('list-item')
+// console.log(listItem)
+// const clickBtn=document.getElementById('click-btn')
+// console.log(clickBtn)
+// const list=document.querySelectorAll('li')
+// console.log(list)
+// const title=document.getElementById('title')
+// title.textContent='new text'
+const listItem=document.querySelectorAll('.list-item')
 console.log(listItem)
-const clickBtn=document.getElementById('click-btn')
-console.log(clickBtn)
+listItem.forEach((item)=>{
+    item.textContent+=' darsalri'
+})
+const title=document.getElementById('title')
+title.innerHTML=' <i>darslar jadvali</i>'
